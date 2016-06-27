2867aeb44e45c2bdf1fbb7cd32a4aa600965b63f
Hendy Irawan <ceefour666@gmail.com>
2016-06-28 01:10:34 +0700

    New files

---
+++ mobile/typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/angular-ui-router/index.d.ts" />
+/// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/cordova/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/underscore/index.d.ts" />
---
+++ mobile/www/js/services.js
@@ -0,0 +1,4 @@
+/// <reference path="../typings/index.d.ts"/>
+angular.module('starter.services', []);
+
+//# sourceMappingURL=services.js.map
